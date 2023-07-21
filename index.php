7a8805c49a257a7c59eeaf86f9470b039c4b55d9
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2023-07-21 18:40:31 +0200

    attribution des classes et id différents pour les svg pour évité les conflit de couleurs

--- index.php
+++ index.php
@@ -263,9 +263,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 //     echo "</svg>";
                 // }
 
-                            echo "<svg class='temp_cloud_few'>";   
-                                echo "<use xlink:href='public/cloud_few/cloud_few.svg#cloud_few'/>";
-                            echo "</svg>";
+                        echo "<svg class='temp_cloud icon-no-stroke'>";   
+                            echo "<use xlink:href='public/cloud/cloud.svg#cloud'/>";
+                        echo "</svg>";
                 
                     echo "<div class='temp_txt'>";
                         echo "<p class='txt_temperature'>" . $_SESSION['temperature'] . "°C</p>";
