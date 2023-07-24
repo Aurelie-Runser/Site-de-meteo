4e17a11d16f3f78e9d45f4c82606a491d75d4fb1
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2023-07-24 16:25:04 +0200

    ajout d'un favicon

--- index.php
+++ index.php
@@ -122,7 +122,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mon site météo</title>
+    <title>Mon site de Météo</title>
+    <link rel="shortcut icon" type="image/png" href="favicon.ico"/>
 
     <link href="style.css" rel="stylesheet">
 
